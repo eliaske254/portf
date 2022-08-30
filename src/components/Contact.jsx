a9e21396dd8ae3ed32f206bd6f301a6826f2e8be
Elias Kirui <eliaskips1@gmail.com>
2022-08-30 12:26:57 +0300

    Add projects & links to social media accounts

---
+++ src/components/Contact.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-slate-900 flex justify-center items-center p-4"
+    >
+      <form
+        method="POST"
+        action="https://getform.io/f/03ea4bdf-bb21-43a3-a620-fa4b471c6200"
+        className="flex flex-col max-w-[600px] w-full"
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">
+            // Shoot me an email on the form below
+          </p>
+        </div>
+        <input
+          className="bg-[#ccd6f6] p-2"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="bg-[#ccd6f6] p-2 my-2"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <textarea
+          className="bg-[#ccd6f6] p-2"
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto">
+          Let's collaborate
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/components/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-slate-900">
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-indigo-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Elias kirui
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Full Stack Ruby on Rails Engineer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I specialize in building (and occasionally designing) exceptional
+          digital experiences. Currently, I'm focused on building accessible,
+          human-centered products
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-indigo-600 hover:border-green-700 rounded-lg">
+            View my work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
